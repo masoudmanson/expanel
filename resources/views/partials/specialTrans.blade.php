19b634681aa76ded44e0f0fddb2a3689c042130b
Masoud Amjadi <masoudmanson@gmail.com>
2017-11-14 14:29:47 +0330

    Code Clean Up

--- resources/views/partials/specialTrans.blade.php
+++ resources/views/partials/specialTrans.blade.php
@@ -1,29 +1,29 @@
-<div class="row number-stats margin-bottom-30">
-    <div class="col-md-6 col-sm-6 col-xs-6">
-        <div class="stat-left">
-            <div class="stat-chart">
-                <div id="sparkline_bar"></div>
-            </div>
-            <div class="stat-number">
-                <div class="title" style="padding-bottom: 5px"> جمع پرداختی های <span class="perName">امروز</span>
-                    <small>(ریال)</small>
-                </div>
-                <div class="number">{{ number_format($today['sum']) }}</div>
-            </div>
-        </div>
-    </div>
-    <div class="col-md-6 col-sm-6 col-xs-6">
-        <div class="stat-right">
-            <div class="stat-chart">
-                <div id="sparkline_bar2"></div>
-            </div>
-            <div class="stat-number">
-                <div class="title" style="padding-bottom: 5px"> تعداد تراکنش های <span class="perName">امروز</span> </div>
-                <div class="number">{{ number_format($today['count']) }}</div>
-            </div>
-        </div>
-    </div>
-</div>
+{{--<div class="row number-stats margin-bottom-30">--}}
+    {{--<div class="col-md-6 col-sm-6 col-xs-6">--}}
+        {{--<div class="stat-left">--}}
+            {{--<div class="stat-chart">--}}
+                {{--<div id="sparkline_bar"></div>--}}
+            {{--</div>--}}
+            {{--<div class="stat-number">--}}
+                {{--<div class="title" style="padding-bottom: 5px"> جمع پرداختی های <span class="perName">امروز</span>--}}
+                    {{--<small>(ریال)</small>--}}
+                {{--</div>--}}
+                {{--<div class="number">{{ number_format($today['sum']) }}</div>--}}
+            {{--</div>--}}
+        {{--</div>--}}
+    {{--</div>--}}
+    {{--<div class="col-md-6 col-sm-6 col-xs-6">--}}
+        {{--<div class="stat-right">--}}
+            {{--<div class="stat-chart">--}}
+                {{--<div id="sparkline_bar2"></div>--}}
+            {{--</div>--}}
+            {{--<div class="stat-number">--}}
+                {{--<div class="title" style="padding-bottom: 5px"> تعداد تراکنش های <span class="perName">امروز</span> </div>--}}
+                {{--<div class="number">{{ number_format($today['count']) }}</div>--}}
+            {{--</div>--}}
+        {{--</div>--}}
+    {{--</div>--}}
+{{--</div>--}}
 <div class="table-scrollable table-scrollable-borderless">
     @if(count($today['special']) > 0)
         <table class="table table-hover table-light">
