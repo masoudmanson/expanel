c07d5f88a28607bf62a76e000545a70b96821ca8
Masoud Amjadi <masoudmanson@gmail.com>
2017-12-25 16:05:34 +0330

    Final Changes

--- app/Http/Requests/AuthRequest.php
+++ app/Http/Requests/AuthRequest.php
@@ -26,7 +26,7 @@ class AuthRequest extends FormRequest
         return [
             'firstname' => 'required',
             'lastname' => 'required',
-            'identity_number' => 'required',
+            'identity_number' => 'required|unique:authorized,identity_number',
             'mobile' => 'required',
         ];
     }
